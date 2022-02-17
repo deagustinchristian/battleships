d6228b02cefcc7f3f17490db80203f80f6558d41
deagustinchristian <deagustinchristian@gmail.com>
2022-02-17 20:04:54 +0000

    Add class board and function to convert letters to numbers for the column display

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from random import randint
 
-score = {"computer": 0, "player": 0}
+import os
 
-
-class Board
-
-    def __init__(self, size, num_ships name, type):
-        self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)]
-        
+class Board:
+    def __init__(self.board):
+        self.board = board
+"""
+Below function converts numbers to letters when displaying the columns
+"""
+    def get_letters_to_numbers():
+        letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
+        return letters_to_numbers
