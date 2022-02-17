73cc08e77afd610fd947b0b6535731e1e10c96ae
deagustinchristian <deagustinchristian@gmail.com>
2022-02-17 21:51:18 +0000

    Add function for player to choose row and column, if choice not between 1-5 or A-E, tells player choice not valid and asks for input again

--- run.py
+++ run.py
@@ -34,3 +34,24 @@ if so it continues to look until it finds a free spot
                 self.x_row, self.y_column = random.randint(0, 4), random.randint(0, 4)
             self.board[self.x_row][self.y_column] = "X"
         return self.board
+"""
+Below function asks the player to enter a row and a column,
+it checks so that the player enters a valid row number between 1-5 and a 
+column letter A-E 
+"""
+    def get_player_input(self):
+        try:
+            x_row = input("Enter the row of the ship: ")
+            while x_row not in "12345":
+                print("Not a valid choice, please select a valid row")
+                x_row = input("Enter the row of the ship: ")
+
+            y_column = input("Enter the column letter of the ship: ").upper()
+            while y_column not in "ABCDE":
+                print("Not a valid choice, please select a valid column")
+                y_column = input("Enter the column letter of the ship: ").upper()
+            return int(x_row) - 1, Board.get_letters_to_numbers()[y_column]
+        except ValueError and KeyError:
+            print("Not a valid input")
+            return self.get_player_input()
+    
