320b60288eeedbc2aea728b8a6d7fad990e149ef
deagustinchristian <deagustinchristian@gmail.com>
2022-02-18 22:21:17 +0000

    Add functions to check hit or miss, check so player input not duplicate, game start with 15 missiles, fix indentation issues

--- run.py
+++ run.py
@@ -1,69 +1,101 @@
-from random import randint
-
+import random
 import os
 
 class Board:
-    def __init__(self, board):
-        self.board = board
-"""
-Below function converts numbers to letters when displaying the columns
-"""
-    def get_letters_to_numbers():
-        letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
-        return letters_to_numbers
+  def __init__(self, board):
+    self.board = board
 
-    def print_board(self):
-        print("  A B C D E")
-        row_number = 1
-        for row in board:
-            print("%d|%s|" % (row_number, "|".join(row))) # This idea i got from dmoisset, see README
-            row_number += 1
+#Below function converts numbers to letters when displaying the columns
+
+  def get_letters_to_numbers():
+    letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
+    return letters_to_numbers
+
+  def print_board(self):
+    print("  A B C D E")
+    row_number = 1
+    for row in self.board:
+        print("%d|%s|" % (row_number, "|".join(row))) # This idea i got from dmoisset, see README
+        row_number += 1
 
 class Ships:
-    def __init__(self, board):
-        self.board = board
-"""
-Below function places the computers 6 ships randomly on the board and checks 
-if there is already a ship placed, 
-if so it continues to look until it finds a free spot
-"""
-    def create_ships(self):
-        for i in range(6):
-            self.x_row, self.y_column = random.randint(0, 4), random.randint(0, 4)
-            while self.board[self.x_row][self.y_column] == "X":
-                self.x_row, self.y_column = random.randint(0, 4), random.randint(0, 4)
-            self.board[self.x_row][self.y_column] = "X"
-        return self.board
-"""
-Below function asks the player to enter a row and a column,
-it checks so that the player enters a valid row number between 1-5 and a 
-column letter A-E 
-"""
-    def get_player_input(self):
-        try:
-            x_row = input("Enter the row of the ship: ")
-            while x_row not in "12345":
-                print("Not a valid choice, please choose 1, 2, 3, 4, or 5")
-                x_row = input("Enter the row of the ship: ")
+  def __init__(self, board):
+    self.board = board
+  """
+  Below function places the computers 6 ships randomly on the board and checks 
+  if there is already a ship placed, 
+  if so it continues to look until it finds a free spot
+  """
+  def create_ships(self):
+    for i in range(6):
+      self.x_row, self.y_column = random.randint(0, 4), random.randint(0, 4)
+      while self.board[self.x_row][self.y_column] == "X":
+        self.x_row, self.y_column = random.randint(0, 4), random.randint(0, 4)
+      self.board[self.x_row][self.y_column] = "X"
+    return self.board
+  """
+  Below function asks the player to enter a row and a column,
+  it checks so that the player enters a valid row number between 1-5 and a 
+  column letter A-E 
+  """
+  def get_player_input(self):
+    try:
+      x_row = input("Enter the row of the ship: ")
+      while x_row not in "12345":
+          print("Not a valid choice, please choose 1, 2, 3, 4, or 5")
+          x_row = input("Enter the row of the ship: ")
 
-            y_column = input("Enter the column letter of the ship: ").upper()
-            while y_column not in "ABCDE":
-                print("Not a valid choice, please choose A, B, C, D, or E")
-                y_column = input("Enter the column letter of the ship: ").upper()
-            return int(x_row) - 1, Board.get_letters_to_numbers()[y_column]
-        except ValueError and KeyError:
-            print("Not a valid input")            
-            return self.get_player_input()
+      y_column = input("Enter the column letter of the ship: ").upper()
+      while y_column not in "ABCDE":
+          print("Not a valid choice, please choose A, B, C, D, or E")
+          y_column = input("Enter the column letter of the ship: ").upper()
+      return int(x_row) - 1, Board.get_letters_to_numbers()[y_column]
+    except ValueError and KeyError:
+      print("Not a valid input")            
+      return self.get_player_input()
     
-    def sunk_ships(self):
-        sunk_ships = 0
-        for row in self.board:
-          for column in row:
-            if column == "X":
-              sunk_ships += 1
-        return sunk_ships
+  def sunk_ships(self):
+    sunk_ships = 0
+    for row in self.board:
+      for column in row:
+        if column == "X":
+          sunk_ships += 1
+    return sunk_ships
 
 def new_game():
+  computer_board = Board([[" "] * 5 for i in range(5)])
+  player_guess_board = Board([[" "] * 5 for i in range(5)])
+  Ships.create_ships(computer_board)
 
+  #starts game with 15 missiles
+  missiles = 15
+  while missiles > 0:
+    Board.print_board(player_guess_board)
 
-#need to go to work now
+    #Gets the player input and checks so not duplicate
+    player_x_row, player_y_column = Ships.get_player_input(object)
+    
+    while player_guess_board.board[player_x_row][player_y_column] == "-" or player_guess_board.board[player_x_row][player_y_column] == "X":
+      print("You tried this one already!")
+      player_x_row, player_y_column = Ships.get_player_input(object)
+    
+    #checks if its a hit or a miss
+    if computer_board.board[player_x_row][player_y_column] == "X":
+      print("You sunk one of my battleships!")
+      player_guess_board.board[player_x_row][player_y_column] = "X"
+    else:
+      print("You missed!")
+      player_guess_board.board[player_x_row][player_y_column] = "-"
+
+    if Ships.sunk_ships(player_guess_board) == 6:
+      print("You sunk all 6 battleships! You won the game!")
+      break
+    else:
+      missiles -= 1
+      print(f"You have {missiles} missiles left")
+      if missiles == 0:
+        print("You ran out of missiles, the bad guys got away!")
+        Board.print_board(player_guess_board)
+        break
+
+new_game()
