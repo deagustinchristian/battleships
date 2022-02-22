d46d104a5e25cfb961369d4b7042a0b25332cf2f
deagustinchristian <deagustinchristian@gmail.com>
2022-02-22 08:43:37 +0000

    Fix the last validation

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 import random
-import os
+
+
+def get_letters_to_numbers():
+    letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
+    return letters_to_numbers
 
 
 class Board:
     def __init__(self, board):
         self.board = board
 
-# Below function converts numbers to letters when displaying the columns
-
-    def get_letters_to_numbers():
-        letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
-        return letters_to_numbers
+    # Below function converts numbers to letters when displaying the columns
 
     # Part of this code I got from dmoisset, see README
     def print_board(self):
@@ -24,12 +24,14 @@ class Board:
 class Ships:
     def __init__(self, board):
         self.board = board
+
     """
     Below function places the computers 6 ships
     randomly on the board and checks
     if there is already a ship placed,
     if so it continues to look until it finds a free spot
     """
+
     def create_ships(self):
         for i in range(6):
             self.x_row, self.y_column = (random.randint(0, 4),
@@ -39,27 +41,29 @@ class Ships:
                                              random.randint(0, 4))
             self.board[self.x_row][self.y_column] = "X"
         return self.board
+
     """
     Below function asks the player to enter a row and a column,
     it checks so that the player enters a valid row number between 1-5 and a
     column letter A-E
     """
+
     # fix input validation, blank input!!!
     # restrict to one character/number test abcde 12345
     def get_player_input(self):
         try:
             x_row = input("Enter the row of the ship: \n")
-            while x_row not in "12345":
+            while x_row not in list(["1", "2", "3", "4", "5"]):
                 print("Not a valid choice, please choose 1, 2, 3, 4, or 5")
                 x_row = input("Enter the row of the ship: \n")
             # .upper makes it possible to allow
             # player to input lower case letters, a-e
             y_column = input("Enter the column letter of the ship: \n").upper()
-            while y_column not in "ABCDE":
+            while y_column not in list(["A", "B", "C", "D", "E"]):
                 print("Not a valid choice, please choose A, B, C, D, or E")
                 y_column = (input("Enter the column letter of the ship: \n")
                             .upper())
-            return int(x_row) - 1, Board.get_letters_to_numbers()[y_column]
+            return int(x_row) - 1, get_letters_to_numbers()[y_column]
         except ValueError and KeyError:
             print("Not a valid input")
             return self.get_player_input()
@@ -90,14 +94,14 @@ def new_game():
     while missiles > 0:
         Board.print_board(player_guess_board)
 
-    # Gets the player input and checks so not duplicate
+        # Gets the player input and checks so not duplicate
         player_x_row, player_y_column = Ships.get_player_input(object)
 
         while player_guess_board.board[player_x_row][player_y_column] == "-" or player_guess_board.board[player_x_row][player_y_column] == "X":
             print("You tried this one already!")
             player_x_row, player_y_column = Ships.get_player_input(object)
 
-    # checks if its a hit or a miss
+        # checks if its a hit or a miss
         if computer_board.board[player_x_row][player_y_column] == "X":
             print("You sunk one of my battleships!")
             player_guess_board.board[player_x_row][player_y_column] = "X"
@@ -121,5 +125,6 @@ def new_game():
         new_game()
     else:
         print("Thanks for playing!")
+
 
 new_game()
